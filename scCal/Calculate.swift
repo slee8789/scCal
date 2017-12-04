6d9d72f64e29e5f1663eedb03eea4d004a6126ef
leesc <slee8789@gmail.com>
2017-12-05 00:35:07 +0900

    first commit

---
+++ scCal/Calculate.swift
@@ -0,0 +1,9 @@
+//
+//  Calculate.swift
+//  scCal
+//
+//  Created by 이승철 on 2017. 11. 28..
+//  Copyright © 2017년 이승철. All rights reserved.
+//
+
+import Foundation
---
+++ scCal/RoundButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundButton.swift
+//  scCal
+//
+//  Created by 이승철 on 2017. 11. 26..
+//  Copyright © 2017년 이승철. All rights reserved.
+//
+
+import UIKit
+
+class RoundButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
