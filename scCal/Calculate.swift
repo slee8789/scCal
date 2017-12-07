4234653995af0ee2fd36ed02cd0de689558b3570
leesc <slee8789@gmail.com>
2017-12-08 01:05:56 +0900

    기본 연산 구현

--- scCal/Calculate.swift
+++ scCal/Calculate.swift
@@ -6,4 +6,72 @@
 //  Copyright © 2017년 이승철. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Calculate {
+    
+    var result:Double = 0
+    var preResult:Double = 0
+    var numberCache:String = ""
+    var oneOperator:String = ""
+    var twoOperator:String = ""
+    var isPointNumber:Bool = false
+    
+    func allClearModel() -> String {
+        print("allClearModel")
+        result = 0
+        preResult = 0
+        numberCache = ""
+        twoOperator = ""
+        return String(result)
+    }
+    
+    func changeSignModel() -> String  {
+        print("changeSignModel")
+        result *= -1
+        numberCache = String(result)
+        return numberCache
+    }
+    
+    func setNumberModel(numberStr: String) -> String {
+        numberCache.append(numberStr)
+        result = Double(numberCache)!
+        return numberCache
+    }
+    
+    func setOneOperatorModel(operatorStr: String) -> String {
+        oneOperator = operatorStr
+        switch oneOperator {
+        case "AC":
+            return allClearModel()
+        case "+/r-":
+            return allClearModel()
+        case "%":
+            preResult = preResult == 0 ? result * 0.01 : preResult * 0.01
+            return String(preResult)
+        case "=":
+            switch twoOperator {
+            case "÷":preResult = result == 0 ? preResult : preResult == 0 ? result : preResult / result
+            case "×":preResult = result == 0 ? preResult : preResult == 0 ? result : preResult * result
+            case "+":preResult += result
+            case "-":preResult -= result
+            default:preResult = result
+            }
+            result = 0;
+            numberCache = ""
+            print("equal preResult \(preResult), result \(result), twoOperator \(twoOperator)" )
+            return String(preResult)
+        default: return ""
+        }
+    }
+    
+    func setTwoOperatorModel(operatorStr: String) -> String {
+        if twoOperator == "" {twoOperator = operatorStr}
+        let returnValue = setOneOperatorModel(operatorStr:"=")
+        twoOperator = operatorStr
+        print("twoOper preResult \(preResult), result \(result), twoOperator \(twoOperator)" )
+        return returnValue
+    }
+    
+}
+
--- scCal/ViewController.swift
+++ scCal/ViewController.swift
@@ -10,14 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let calculator = Calculate()
+    
+    @IBOutlet weak var displayView: UITextView!
+    
+    @IBAction func numberClick(_ sender : RoundButton) {
+        setNumber(numberStr: sender.currentTitle!)
+    }
+    
+    @IBAction func oneOperatorClick(_ sender : RoundButton) {
+        displayView.text = setOneOperator(operatorStr: sender.currentTitle!)
+    }
+    
+    @IBAction func twoOperatorClick(_ sender : RoundButton) {
+        displayView.text = setTwoOperator(operatorStr: sender.currentTitle!)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+	
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func setNumber(numberStr: String) {
+        displayView.text = calculator.setNumberModel(numberStr: numberStr)
+    }
+    
+    func setOneOperator(operatorStr: String) -> String {
+        return calculator.setOneOperatorModel(operatorStr: operatorStr)
+    }
+    
+    func setTwoOperator(operatorStr: String) -> String {
+        return calculator.setTwoOperatorModel(operatorStr: operatorStr)
     }
 
 
